1fac9d244c8634d1667190361f7208f7d6382530
Alren54 <hollyali1234@hotmail.com>
2024-05-12 17:11:02 +0300

    seri psuh

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartButton() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    public void ExitButton() => Application.Quit();
+}
