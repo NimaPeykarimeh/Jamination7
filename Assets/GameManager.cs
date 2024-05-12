4d689ea99b98b3a1264f801d31dd94e8554d76d2
Alren54 <hollyali1234@hotmail.com>
2024-05-12 05:11:20 +0300

    cat score changes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI totalScoreText;
+    public void CreatePointText(TextMeshProUGUI catScoreText)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
